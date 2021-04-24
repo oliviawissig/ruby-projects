983d35921c90a48f616529338ef11c67eab421d4
Olivia Wissig <oliviawissig@gmail.com>
2021-04-24 16:29:07 -0700

    added some context to program

--- StockPicker/stock.rb
+++ StockPicker/stock.rb
@@ -1,11 +1,17 @@
+## Short program that returns lowest and highest stock prices from an array
+## returns an array with the lowest price at index 0 and highest price at index 1
 def stock_picker(array)
   low, high = array[0], 0
-  array.each do |num|
+  array.each_with_index do |num, index|
     low = num if num < low
     high = num if num > high
   end
-  print [low, high], "\n"
+  [low, high]
 end
 
-prices = [17,3,6,9,15,8,6,1,10]
-stock_picker(prices)
+prices = [26.74,40.96,16.99,24.34,20.02,55.24,36.84,29.10]
+buy_sell = stock_picker(prices)
+
+print "Prices: ", prices, "\n"
+print "The best time to buy is $" + (buy_sell[0]).to_s + ". "
+print "The best time to sell is $" + (buy_sell[1]).to_s + "\n"
